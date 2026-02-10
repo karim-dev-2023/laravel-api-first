8abbf3c2ae3b9c61c22ccae50505cdb04d4bd6b3
Karim <karim.ayoub.job@gmail.com>
2026-02-10 14:37:56 +0100

    Using match

--- routes/api_v2.php
+++ routes/api_v2.php
@@ -10,12 +10,13 @@ Route::post('/login', [UserController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [UserController::class, 'logout']);
 
-    
     Route::post('/books', [BookController::class, 'store']);
-    Route::put('/books/{book}', [BookController::class, 'update']);
-    Route::patch('/books/{book}', [BookController::class, 'update']);
+
+    Route::match(['put', 'patch'], '/books/{book}', [BookController::class, 'update']);
+
     Route::delete('/books/{book}', [BookController::class, 'destroy']);
 });
+
 
  
 Route::get('/books', [BookController::class, 'index']);
